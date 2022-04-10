b75bf6b627c15d9ff09a6a75cfc7624b52a9b8b7
Mariusz Perzynski <m.perzyk@interia.pl>
2022-04-10 12:07:49 +0200

    example ready to use

--- cnc.cpp
+++ cnc.cpp
@@ -20,3 +20,37 @@ class kolo
 
     void przetwarzaj();
     };
+
+    void kolo::obliczaj()
+    {
+        pole = PI * pow(promien,2.0);
+        obwod = 2 * PI * promien;
+    }
+
+    void kolo::pokaz_wyniki()
+    {
+        printf("\n Parametry kola: ");
+        printf("\n Promien=%.3lE", promien);
+        printf("\n Pole=%.3lE", pole);
+        printf("\n Obwod=%3lE\n\n", obwod);
+    }
+
+    void kolo::przetwarzaj()
+    {
+    obliczaj();
+    pokaz_wyniki();
+    }
+
+    int main()
+    {
+        kolo k1;
+        podaj_promien(k1, 3.0);
+        k1.przetwarzaj();
+        getch();
+        return 0;
+        }
+        
+    void podaj_promien(kolo &k, double r)
+    {
+        k.promien=r;
+    }
