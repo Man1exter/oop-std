6b0b8b443a528aea2aab0a6d7d05c98bdc471a03
Mariusz Perzynski <m.perzyk@interia.pl>
2022-04-10 12:03:28 +0200

    modyfi.. klas + create

--- cnc.cpp
+++ cnc.cpp
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+
+const float PI = 3.14; // zmienna globalna...
+
+class kolo
+{
+    friend void podaj_promien(kolo &, double);
+
+    private: //prywatne..
+
+    double promien;
+    double pole, obwod;
+    void podaj_promien();
+    void obliczaj();
+    void pokaz_wyniki();
+
+    public: //publiczne..
+
+    void przetwarzaj();
+    };
